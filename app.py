f5ea484fb36e869e577d4a6422b48015737765c4
knowledge-platformpk <158771938+knowledge-platformpk@users.noreply.github.com>
2024-09-08 22:58:59 +0500

    Updated PDF formatting
    Display without contextual view

--- app.py
+++ app.py
@@ -1008,7 +1008,8 @@ def main():
             pdf_bytes = file.read()
         
         base64_pdf = base64.b64encode(pdf_bytes).decode("utf-8")
-        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="720" height="1280" type="application/pdf"></iframe>'
+        # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1280" height="1280" type="application/pdf"></iframe>'
+        pdf_display = f'''<iframe src="data:application/pdf;base64,{base64_pdf}#toolbar=0&view=fitH" width="1280" height="1280" style="border: none;"></iframe>'''
         st.markdown(pdf_display, unsafe_allow_html=True)
 
             
