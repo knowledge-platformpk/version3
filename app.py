50fe0639f41c02d1a5a506e6b5cb3182a1e8d1e4
knowledge-platformpk <158771938+knowledge-platformpk@users.noreply.github.com>
2024-09-19 02:06:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -984,8 +984,8 @@ def main():
         Map.to_streamlit(height=600)
 
         if 'chart' in st.session_state and st.session_state['chart'] is not None:
-            # st.pyplot(st.session_state['chart'])
-            st.components.v1.html(st.session_state['chart'], height=1000)
+            st.pyplot(st.session_state['chart'])
+            # st.components.v1.html(st.session_state['chart'], height=1000) # for google charts API comment above unvomment this
 
 
 
